8c7b9c54ff097ce363c16fb2d5ac34d219260edf
millertim83 <millertim83@gmail.com>
2025-07-15 20:46:15 -0400

    Adjust initial fetch callback and create filter buttons dynamically

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,26 +1,45 @@
 const gallery = document.getElementById("gallery");
+let allWorks = [];
+const filtersContainer = document.getElementById("filters");
 
 fetch("http://localhost:5678/api/works")
-  .then(response => response.json())
-  .then(works => {
-    works.forEach(work => {
-      // Create elements
-      const figure = document.createElement("figure");
-
-      const img = document.createElement("img");
-      img.src = work.imageUrl;
-      img.alt = work.title;
-
-      const caption = document.createElement("figcaption");
-      caption.textContent = work.title;
-
-      // Assemble and append
-      figure.appendChild(img);
-      figure.appendChild(caption);
-      gallery.appendChild(figure);
+    .then(response => response.json())
+    .then(works => {
+        allWorks = works;
+        renderGallery(works);
     });
-  })
-  .catch(error => {
-    console.error("Error loading gallery:", error);
-    gallery.innerHTML = "<p>Failed to load portfolio items.</p>";
-  });
+
+function renderGallery(works) {
+    gallery.innerHTML = "";
+    works.forEach(work => {
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        img.src = work.imageUrl;
+        img.alt = work.title;
+        const caption = document.createElement("figcaption");
+        caption.textContent = work.title;
+
+        figure.appendChild(img)
+        figure.appendChild(caption);
+        gallery.appendChild(figure);
+    });
+    }
+
+fetch("http://localhost:5678/api/categories") 
+    .then(response => response.json())
+    .then(categories => {
+        const allBtn = document.createElement("button");
+        allBtn.textContent = "All";
+        filtersContainer.appendChild(allBtn);
+
+        categories.forEach(category => {
+            const btn = document.createElement("button");
+            btn.textContent = category.name;
+            filtersContainer.appendChild(btn);
+        })
+    })
+
+
+function filterCategory(category) {
+    
+}
