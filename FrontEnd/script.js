348321d938903d95c6a7a69b082e6f52ed5a5f03
millertim83 <millertim83@gmail.com>
2025-07-19 09:49:26 -0400

    Add filter buttons

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,6 +2,7 @@ const gallery = document.getElementById("gallery");
 let allWorks = [];
 const filtersContainer = document.getElementById("filters");
 
+
 fetch("http://localhost:5678/api/works")
     .then(response => response.json())
     .then(works => {
@@ -25,21 +26,45 @@ function renderGallery(works) {
     });
     }
 
-fetch("http://localhost:5678/api/categories") 
-    .then(response => response.json())
-    .then(categories => {
-        const allBtn = document.createElement("button");
-        allBtn.textContent = "All";
-        filtersContainer.appendChild(allBtn);
 
-        categories.forEach(category => {
-            const btn = document.createElement("button");
-            btn.textContent = category.name;
-            filtersContainer.appendChild(btn);
-        })
-    })
+function addFilterButtons() {
+    fetch("http://localhost:5678/api/categories") 
+        .then(response => response.json())
+        .then(categories => {
+            const allBtn = document.createElement("button");
+            allBtn.textContent = "All";
+            allBtn.classList.add("filter-button");
+            allBtn.dataset.id = "";
+            filtersContainer.appendChild(allBtn);
 
+            categories.forEach(category => {
+                const btn = document.createElement("button");
+                btn.classList.add("filter-button");
+                btn.textContent = category.name;
+                btn.dataset.id = category.id;
+                filtersContainer.appendChild(btn);
+            });
 
-function filterCategory(category) {
-    
+            addFilterButtonEvents();
+    });
 }
+
+function addFilterButtonEvents() {
+    const filterButtons = document.querySelectorAll(".filter-button");
+    filterButtons.forEach(button => {
+        button.addEventListener("click", event => {
+            const categoryId = button.dataset.id;
+
+            if (!categoryId) {
+                renderGallery(allWorks);
+            } else {
+                const filtered = allWorks.filter(work => work.categoryId == categoryId);
+                renderGallery(filtered);
+            }
+        });
+    });
+}
+
+addFilterButtons();
+
+
